687836e243c6c3bbcfea5ef181b024a3cf1ac394
MinwooPark93 <79454069+MinwooPark93@users.noreply.github.com>
2021-11-25 11:14:53 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const colors = [
+  "#ef5777",
+  "#575fcf",
+  "#4bcffa",
+  "#34e7e4",
+  "#0be881",
+  "#f53b57",
+  "#3c40c6",
+  "#0fbcf9",
+  "#00d8d6",
+  "#05c46b",
+  "#ffc048",
+  "#ffdd59",
+  "#ff5e57",
+  "#d2dae2",
+  "#485460",
+  "#ffa801",
+  "#ffd32a",
+  "#ff3f34"
+];
+const body = document.querySelector("body");
+const colorBtn = document.querySelector("button");
+
+function setGradientBgcolor() {
+  const randomColor1 = colors[Math.floor(Math.random() * colors.length)];
+  const randomColor2 = colors[Math.floor(Math.random() * colors.length)];
+  console.log(randomColor1, randomColor2);
+  body.style.background = `linear-gradient(to right, ${randomColor1}, ${randomColor2})`;
+}
+
+colorBtn.addEventListener("click", setGradientBgcolor);
